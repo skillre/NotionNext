c3dec00b8870177e7b5a25e3713e93c710591612
skillre <34838199+skillre@users.noreply.github.com>
2025-01-16 13:18:38 +0800

    Update ChatBase.js

--- components/ChatBase.js
+++ components/ChatBase.js
@@ -1,24 +1,27 @@
-import { siteConfig } from '@/lib/config';
+import { siteConfig } from '@/lib/config'
 
+/**
+ * 这是一个嵌入组件，可以在任意位置全屏显示您的chat-base对话框
+ * 暂时没有页面引用
+ * 因为您可以直接用内嵌网页的方式放入您的notion中 https://www.chatbase.co/chatbot-iframe/${siteConfig('CHATBASE_ID')}
+ */
 export default function ChatBase() {
   if (!siteConfig('CHATBASE_ID')) {
-    return <></>;
+    return <></>
   }
 
-  const baseUrl = `${siteConfig('CHATBASE_ID')}`;
-
-  return (
+   return (
     <div>
       <script>
         {`
           window.difyChatbotConfig = {
             token: 'tpn8UiMlmLEnZq2x',
-            baseUrl: '${baseUrl}'
+            baseUrl: 'https://sknote.skillre.online:44335'
           }
         `}
       </script>
       <script
-        src="${baseUrl}/embed.min.js"
+        src="https://sknote.skillre.online:44335/embed.min.js"
         id="tpn8UiMlmLEnZq2x"
         defer
       ></script>
